22e7da08f76450bf9144c6d455834a595762fc47
andoudou <andoudou@qq.com>
2017-08-21 21:54:30 +0800

    fullpage

---
+++ website/portfolio/JavaScript/fullpage.js
@@ -0,0 +1,64 @@
+"use strict";
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: '#menu',
+        lockAnchors: true,
+        anchors: ['firstPage', 'secondPage'],
+        navigation: false,
+        navigationPosition: 'top',
+        navigationTooltips: ['', ''],
+        showActiveTooltip: false,
+        slidesNavigation: false,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 600,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 500,
+        scrollBar: true,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        interlockedSlides: false,
+        dragAndMove: false,
+        offsetSections: true,
+        resetSliders: false,
+        fadingEffect: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowReset: null,
+        scrollOverflowOptions: true,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        lazyLoading: true,
+
+        //events
+        onLeave: function(index, nextIndex, direction) {},
+        afterLoad: function(anchorLink, index) {},
+        afterRender: function() {},
+        afterResize: function() {},
+        afterResponsive: function(isResponsive) {},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex) {}
+    });
+});
--- website/portfolio/JavaScript/home.js
+++ website/portfolio/JavaScript/home.js
@@ -19,6 +19,7 @@ var scroll = function() {
         $('.headline').css("top", "50vmin");
         $('.baudelaire').css("opacity", "0");
         $('.baudelaire').css("margin-top", "60vh");
+        $('#photoinfo').css("opacity", "0");
     } else {
         $('#navibar').css("position", "absolute");
         $('#navibar').css("background", "none");
@@ -36,6 +37,7 @@ var scroll = function() {
         $('.headline').css("top", "42vmin");
         $('.baudelaire').css("opacity", "1");
         $('.baudelaire').css("margin-top", "45vh");
+        $('#photoinfo').css("opacity", "1");
     }
 };
 
@@ -88,67 +90,4 @@ $(document).ready(function() {
     $img.hide().each(function(index) {
         $(this).delay(800 * index).fadeToggle(700);
     });*/
-});
-
-$(document).ready(function() {
-    $('#fullpage').fullpage({
-        //Navigation
-        menu: '#menu',
-        lockAnchors: true,
-        anchors: ['firstPage', 'secondPage'],
-        navigation: false,
-        navigationPosition: 'top',
-        navigationTooltips: ['', ''],
-        showActiveTooltip: false,
-        slidesNavigation: false,
-        slidesNavPosition: 'bottom',
-
-        //Scrolling
-        css3: true,
-        scrollingSpeed: 600,
-        autoScrolling: true,
-        fitToSection: true,
-        fitToSectionDelay: 500,
-        scrollBar: true,
-        easing: 'easeInOutCubic',
-        easingcss3: 'ease',
-        loopBottom: false,
-        loopTop: false,
-        loopHorizontal: true,
-        continuousVertical: false,
-        continuousHorizontal: false,
-        scrollHorizontally: false,
-        interlockedSlides: false,
-        dragAndMove: false,
-        offsetSections: true,
-        resetSliders: false,
-        fadingEffect: false,
-        normalScrollElements: '#element1, .element2',
-        scrollOverflow: false,
-        scrollOverflowReset: null,
-        scrollOverflowOptions: true,
-        touchSensitivity: 15,
-        normalScrollElementTouchThreshold: 5,
-        bigSectionsDestination: null,
-
-        //Accessibility
-        keyboardScrolling: true,
-        animateAnchor: true,
-        recordHistory: true,
-
-        //Custom selectors
-        sectionSelector: '.section',
-        slideSelector: '.slide',
-
-        lazyLoading: true,
-
-        //events
-        onLeave: function(index, nextIndex, direction) {},
-        afterLoad: function(anchorLink, index) {},
-        afterRender: function() {},
-        afterResize: function() {},
-        afterResponsive: function(isResponsive) {},
-        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {},
-        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex) {}
-    });
 });
